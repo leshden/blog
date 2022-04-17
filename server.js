b71d165ff8055b73bf10c1fff8cc34c5449f396c
Viktor Denikin <leshden@yandex.ru>
2022-04-17 22:44:06 +0300

    styles

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 function callback(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  let readStream = fs.createReadStream(__dirname + '/public/index.html','utf8');
-  readStream.pipe(response);
+  if (request.url === "/") {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    let readStream = fs.createReadStream(__dirname + '/public/index.html','utf8');
+    readStream.pipe(response);
+  } else if (request.url === "/styles.css") {
+    response.writeHead(200, {'Content-Type': 'text/css'});
+    let readStream = fs.createReadStream(__dirname + '/public/styles.css','utf8');
+    readStream.pipe(response);
+  } else {
+    response.writeHead(404, {'Content-Length':'0'});
+    response.end ();
+  }
 }
 
 const  server = http.createServer(callback);
